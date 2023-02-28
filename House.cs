f06d299f255e1298348532226bc442cf36b03f2f
HarriHiie <89829583+HarriHiie@users.noreply.github.com>
2023-02-28 10:45:17 +0200

    Add files via upload

---
+++ House.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum");
+
+            Console.WriteLine(Majad.Singlehouse);
+
+            int Maja = (int)Majad.Apartment;
+            Console.WriteLine(Maja);
+
+            var ma = (Majad)6;
+            Console.WriteLine(ma);
+        }
+    }
+
+    public enum Majad
+    {
+        Singlehouse,
+        Coophouse,
+        Townhouse,
+        Multihome,
+        Modularhome,
+        Apartment = 6
+        
+
+    }
+}
